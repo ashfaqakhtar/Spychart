a72ec2558b30cce6007eb91cbebd98772a350a66
Ashfaq Akhtar <ashfaqakhtar@live.com>
2018-03-28 00:01:54 +0530

    Started spychart projectand take input and salutation of the user

---
+++ main.py
@@ -0,0 +1,7 @@
+print 'Hello Buddy!'
+print 'let\'s get start'
+spy_name = raw_input("What is Your spy Name? ")
+print "Welcome " + spy_name + 'Glad to have you with us.'
+spy_salutation = raw_input('What should we call you (Mr. or Ms)?')
+spy_name = spy_salutation + ' ' + spy_name
+print spy_name
